539e52c2b8b06c7c94e9ffdd80e50fbead257c54
marcebenitez2 <marcebenitez0607@gmail.com>
2024-01-25 19:15:48 -0300

    Remove unnecessary content from prueba file

---
+++ app/components/hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <section className="w-full h-screen  flex py-56 flex-col items-center gap-9 text-center">
+      <div className="flex flex-col gap-2">
+        <div className="tracking-tighter text-9xl 2xln:text-9xl lgn:text-8xl mdn:text-7xl smn:text-6xl font-semibold">
+          <h1>Actúa ahora.</h1>
+          <h2 className="text-[#1493f6] font-bold">Crea el futuro.</h2>
+        </div>
+        <span className="text-2xl text-zinc-500 text-center">
+          Innovación que inspira, soluciones que impactan
+        </span>
+      </div>
+      <button className="bg-[#1493f6] px-5 py-3 font-semibold rounded-xl hover:bg-sky-700 transition-all duration-300">
+        Comenzar con LumarSoft
+      </button>
+    </section>
+  );
+}
+
+export default Hero;
